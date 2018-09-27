1b3f4aba769fb0fc2cfdc32976659af54c8a4fa2
augus-top <acpluschkat@hotmail.com>
2018-09-27 19:01:46 -0300

    Testing heroku

--- config/mongoConnection.js
+++ config/mongoConnection.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 let keys;
 
 if (!process.env['url_mongo']) {
-  // keys = require('../keys.json');
   console.log('ABC');
+  keys = require('../keys.json');
 }
 
 const url = process.env['url_mongo'] || keys.url_mongo;
